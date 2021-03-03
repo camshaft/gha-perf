7bb410211f2849a8e52320eca14aa7753cdb0a7b
Cameron Bytheway <bytheway.cameron@gmail.com>
2021-03-02 18:09:34 -0800

    add action

---
+++ src/install.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
+import path from 'path'
+import fs from 'fs'
+
+export async function install(version: string): Promise<string> {
+  if (version === 'native') {
+    version = await resolveNative()
+  }
+
+  if (version !== 'master') {
+    version = version.replace(/^v/, '')
+  }
+
+  let cachedPath = tc.find('perf', version)
+
+  if (!cachedPath) {
+    const url = `https://camshaft.github.io/gha-perf/v${version}/perf`
+    core.info(`Installing perf from ${url}`)
+
+    const binPath = await tc.downloadTool(url)
+
+    cachedPath = await tc.cacheFile(binPath, 'perf', 'perf', version)
+  }
+
+  core.info(`adding ${cachedPath} to the executable path`)
+
+  core.addPath(cachedPath)
+
+  const bin = path.join(cachedPath, 'perf')
+
+  // make sure it's actually executable
+  await chmodx(bin)
+
+  return bin
+}
+
+async function resolveNative(): Promise<string> {
+  let out = ''
+  await exec.exec('uname', ['-r'], {
+    listeners: {
+      stdout(buffer) {
+        out += buffer.toString()
+      }
+    }
+  })
+  return out.replace(/ /g, '').split('.').slice(0, 2).join('.')
+}
+
+async function chmodx(bin: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.chmod(bin, 0o775, err => (err ? reject(err) : resolve(void 0)))
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {install} from './install'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const version: string = core.getInput('version')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await install(version)
   } catch (error) {
     core.setFailed(error.message)
   }
